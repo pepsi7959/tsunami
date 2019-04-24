1992c7ac2a4dc332fdffbfd9d9c7b04cf4dc3f5e
pepsi7959 <narongsak.mala@gmail.com>
2019-04-24 17:20:55 +0700

    add api handler

---
+++ clients/api/web.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type App struct {
+	Server   http.Server
+	ServeMux *http.ServeMux
+	Handler  http.Handler
+}
+
+// Help
+func help(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "{}")
+}
+
+func (app *App) Init(port string) {
+
+	app.setRoute()
+
+	app.Server = http.Server{
+		Addr:           ":" + port,
+		Handler:        app.ServeMux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+}
+
+func (app *App) AddApi(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+	app.ServeMux.HandleFunc(pattern, handler)
+}
+
+func (app *App) setRoute() {
+	app.ServeMux = http.NewServeMux()
+	app.ServeMux.HandleFunc("/help", help)
+}
+
+func (app *App) Run() {
+	app.Server.ListenAndServe()
+}
